a6a44ea57fb1eba55311ff0107d46ff5b5ed4706
BlackHIG <black-hig@outlook.com>
2025-11-13 21:54:06 +0800

    fixes big chunks

--- composables/useRenderMd.server.ts
+++
@@ -1,9 +0,0 @@
-import { render } from '~/server/utils/renderMd'
-
-export function useRenderMd() {
-  return {
-    render: async (markdown: string) => {
-      return await render(markdown)
-    }
-  }
-}
